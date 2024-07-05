37f6c5105b070bcc9b217df15c47f21a82081279
tugbaapuhan <tgbapuhan@gmail.com>
2024-07-05 12:50:05 -0500

    email updates

--- js/server.js
+++ js/server.js
@@ -1,14 +1,48 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
-// Serve static files from the root of the project directory
-app.use(express.static(path.join(__dirname, '..')));
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
 
-// Route for serving the index.html file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'html', 'index.html'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static files
+app.use(express.static(__dirname + '/../'));
+
+app.post('/send', (req, res) => {
+    const { name, email: senderEmail, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: email,
+            pass: password,
+        },
+    });
+
+    const mailOptions = {
+        from: senderEmail,
+        to: email,
+        subject: `Contact Form Submission from ${name}`,
+        text: message,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending message');
+        } else {
+            res.status(200).send('Message sent successfully');
+        }
+    });
 });
 
 app.listen(port, () => {
